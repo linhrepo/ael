473fd52d4b669a339b4a27ac18a19e650ae91f72
boycodontp <boy_codontp@yahoo.com>
2015-04-05 21:56:14 +0700

    Update DB

--- DB/DB_Update.sql
+++ DB/DB_Update.sql
@@ -6,3 +6,13 @@ ALTER TABLE truckingservice
 	ADD COLUMN `infoInvoice` VARCHAR(255);
 ALTER TABLE `ael`.`exfeetable` 
 ADD COLUMN `total` decimal(20,2) NULL AFTER `checkByAdmin`
+	----------- Truckingdetail
+ALTER TABLE truckingdetail
+	ADD COLUMN `phuthau1` bigint(20),
+	ADD COLUMN `phuthau2` bigint(20);
+	
+ALTER TABLE truckingdetail ADD FOREIGN KEY (phuthau1) REFERENCES nhathau(id);
+ALTER TABLE truckingdetail ADD FOREIGN KEY (phuthau2) REFERENCES nhathau(id);
+-----------PackageInfo
+ALTER TABLE packageinfo MODIFY COLUMN freeDemDate int;
+ALTER TABLE packageinfo MODIFY COLUMN freeDetDate int;
