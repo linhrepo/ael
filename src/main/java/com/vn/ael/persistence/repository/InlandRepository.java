ac26910a4f34c738e2ae6d8951249d160130a27c
phuongdbk <dangbakhanhphuong187@gmail.com>
2015-08-07 13:15:38 +0700

    Fix inland

--- src/main/java/com/vn/ael/persistence/repository/InlandRepository.java
+++ src/main/java/com/vn/ael/persistence/repository/InlandRepository.java
@@ -19,7 +19,7 @@ public interface InlandRepository extends GenericRepository<Inland> {
 	@Query("SELECT max(e.counting) FROM Inland e WHERE e.docsgeneral.customer.id = :customerId")
 	Integer findMaxCountingByCustomer(@Param("customerId")Long id);
 	//Add Phuc 1.8
-	@Query("SELECT e FROM Inland e left join fetch e.docsgeneral d left join fetch d.contseals c WHERE (e.docsgeneral.customer.id = :customerId or :customerId is null) and "
+	@Query("SELECT distinct e FROM Inland e left join fetch e.docsgeneral d left join fetch d.contseals c WHERE (e.docsgeneral.customer.id = :customerId or :customerId is null) and "
 			+ "(e.docsgeneral.typeOfContainer.id =:typeOfContainer or :typeOfContainer is null) and "
 			+ "(e.docsgeneral.doDelivery =:doDelivery or :doDelivery is null) and "
 			+ "(e.docsgeneral.jobNo like %:jobNo% or :jobNo is null) and "
